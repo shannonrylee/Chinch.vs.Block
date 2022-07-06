aa913e80c005bcf8d0266c7564b3d6afcd4ded3f
Your Actual Name Here <SHA256:hOXSm53Eq3p5Z4sDLk3h/LjyDO3EhgoJf0cCmjpAP04>
2022-07-06 14:47:41 -0400

    testing

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log(`working`);
+const chinchilla = document.querySelector("#chinchilla");
+const block = document.querySelector("#block");
+const score = document.querySelector("#scoreboard");
+
+function jump() {
+  if (chinchilla.classList != "animate") chinchilla.classList.add("animate");
+  setTimeout(function () {
+    chinchilla.classList.remove("animate");
+  }, 500);
+}
+const check = setInterval(function () {
+  score.innerText++;
+  const chinchillaTop = parseInt(
+    window.getComputedStyle(chinchilla).getPropertyValue("top")
+  );
+  const blockLeft = parseInt(
+    window.getComputedStyle(block).getPropertyValue("left")
+  );
+  if (blockLeft < 20 && blockLeft > 0 && chinchillaTop >= 130) {
+    alert("You got a score of: " + score.innerText + "\nPlay Again");
+    location.reload();
+  }
+}, 90);
